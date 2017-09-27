92f790bb1d8287fe4eff299fe3a1be901e2e05e5
Home PC <1193246020@qq.com>
2017-09-28 07:15:42 +0800

    闭包

--- chapter-7/function-expressions.js
+++ chapter-7/function-expressions.js
@@ -32,5 +32,122 @@
     // 不要这样使用函数声明
     if(condition)
     {
-        sayHi = function
+        sayHi = function()
+        {
+            console.log("Hi!");
+        }
+        else
+        {
+            function()
+            {
+                console.log("Yo!");
+            }
+        }
     }
+
+    // 可以这样用函数表达式代替
+    var sayHi;
+    if(condition)
+    {
+        sayHi = function()
+        {
+            console.log("Hi!");
+        };
+    }
+    else
+    {
+        function()
+        {
+            console.log("Yo!");
+        };
+    }
+
+    // 能够把函数作为其他函数的值返回
+    function createComparisonFunction(properName)
+    {
+        return function(object2, object2)
+        {
+            var value1 = object1[properName];
+            var value2 = object2[properName];
+            if (value1 < value2)
+            {
+                return -1;;
+            }
+            else if (value1 > value2)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        };
+    }
+
+    // 7.1 递归
+        function factorial(num)
+        {
+            if (num <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * factorial(num - 1);
+            }
+        }
+
+        // 易出错
+        var anotherFactorial = factorial;
+        factorial = null;
+        console.log(anotherFactorial(4));   // error
+
+        // arguments.callee 指向正在执行的函数
+        function factorial(num)
+        {
+            if (num <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * arguments.callee(num - 1);
+            }
+        }
+
+        // 严格模式下
+        var factorial = (function f(num))
+        {
+            if (num <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * f(num - 1);
+            }
+        }
+
+    // 闭包
+        // 指有权访问另一个函数作用域中的变量
+        // 常见为函数内部创建另一个函数
+        function createComparisonFunction(propertyName)
+        {
+            return function(object1, object2)
+            {
+                var value1 = object1[propertyName];
+                var value2 = object2[propertyName];
+                if (value1 < value2)
+                {
+                    return -1;
+                }
+                else if (value1 > value2)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            };
+        }
