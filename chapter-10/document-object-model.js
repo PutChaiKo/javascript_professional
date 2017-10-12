be0a4c67827d79eed94f874fad36739e47550279
chipsc-computer <1193246020@qq.com>
2017-10-12 17:23:53 +0800

    finish chapter 10, working on 11.

--- chapter-10/document-object-model.js
+++ chapter-10/document-object-model.js
@@ -570,11 +570,67 @@
                 row1.appendChild(cell2_1);
 
                 // 创建第二行
-                var row1 = document.createElement("tr");
-                tbody.appendChild(row1);
-                var cell1_1 = document.createElement("td");
-                cell1_1.appendChild(document.createTextNode("Cell 1,1"));
-                row1.appendChild(cell1_1);
-                var cell2_1 = document.createElement("td");
-                cell2_1.appendChild(document.createTextNode("Cell 2,1"));
-                row1.appendChild(cell2_1);
+                var row2 = document.createElement("tr");
+                tbody.appendChild(row2);
+                var cell1_2 = document.createElement("td");
+                cell1_2.appendChild(document.createTextNode("Cell 1,2"));
+                row2.appendChild(cell1_2);
+                var cell2_2 = document.createElement("td");
+                cell2_2.appendChild(document.createTextNode("Cell 2,2"));
+                row2.appendChild(cell2_2);
+
+                // 表格添加到文档主体中
+                document.body.appendChild(table);
+
+                // <table>元素添加的属性和方法
+                // 重写代码
+                // 创建 table
+                var table = document.createElement("table");
+                table.border = 1;
+                table.width = "100%";
+
+                // 创建 tbody
+                var tbody = document.createElement("tbody");
+                table.appendChild(tbody);
+
+                // 创建第一行
+                tbody.insertRow(0);
+                tbody.rows[0].insertCell(0);
+                tbody.rows[0].cells[0].appendChild(document.createTextNode("Cell 1,1"));
+                tbody.rows[0].insertCell(1);
+                tbody.rows[0].cells[1].appendChild(document.createTextNode("Cell 2,1"));
+
+                // 创建第二行
+                tbody.insertRow(1);
+                tbody.rows[1].insertCell(0);
+                tbody.rows[1].cells[0].appendChild(document.createTextNode("Cell 1,2"));
+                tbody.rows[1].insertCell(1);
+                tbody.rows[1].cells[1].appendChild(document.createTextNode("Cell 2,2"));
+
+                // 将表格添加到文档
+                document.body.appendChild(table);
+
+            // 10.2.4 使用 NodeList
+                // 下述代码产生无限循环
+                var divs = document.getElementsByTagName("div"),
+                    i,
+                    div;
+
+                for (i = 0; i < divs.length; i++)
+                {
+                    div = document.createElement("div");
+                    document.body.appendChild(div);
+                }
+
+                // length 属性初始化外部的变量
+                var divs = document.getElementsByTagName("div"),
+                    i,
+                    len,
+                    div;
+
+                for (i = 0, len = divs.length; i < len; i++) {
+                    div = document.createElement("div");
+                    document.body.appendChild(div);
+                }
+
+                
---
+++ chapter-11/dom-extensions.js
@@ -0,0 +1,157 @@
+// 11 DOM 扩展
+    // 11.1 选择符 API
+        // 11.1.1 querySelector()
+            // 取得 body
+            var body = document.querySelector("body");
+
+            // ID "myDiv"
+            var myDiv = document.querySelector("#myDiv");
+
+            // fisrt class "selected"
+            var selected = document.querySelector(".selected");
+
+            // 取得 class 为"button"的第一个 img 元素
+            var img = document.body.querySelector("img.button");
+
+        // 11.1.2 querySelectorAll()
+            // 类似于 getElementByTagName("em")
+            var ems = document.getElementById("myDiv").querySelectorAll("em");
+
+            // class "selected"
+            var = selected = document.querySelectorAll(".selected");
+
+            // <p> 元素中 <strong>
+            var strongs = document.querySelectorAll("p strong");
+
+            // 取得返回的 NodeList
+            var i, len, strong;
+            for (i = 0, len = strong.length; i < len; i++)
+            {
+                strong = strongs[i];    // 或者 strongs.item(i)
+                stron.className = "important";
+            }
+
+        // 11.1.3 matchesSelector()
+            // 用来匹配dom元素是否匹配某css selector。
+            // 它为一些高级方法的实现提供了基础支持，比如事件代理，parent, closest等。
+
+    // 11.2 遍历元素
+        // 解决不同浏览器对于元素之间空格和注释的不同处理问题
+        var i,
+            len,
+            child = element.fisrtChild;
+        while (child != element.lastChild)
+        {
+            if (child.nodeType == 1)
+            {
+                processChild(child);
+            }
+            child = child.nextSibing;
+        }
+
+        // 可改写为
+        var i,
+            len,
+            child = element.fisrtElementChild;
+        while (child != element.lastElementChild)
+        {
+            processChild(child);
+            child = child.nextSibing;
+        }
+
+    // 11.3 HTML5
+        // 11.3.1 与类相关的扩充
+            // 1.getElementByClassName()，新版本浏览器支持
+                // 取得所有 class 里包含 username 和 current 的元素，不分先后
+                var allCurrentUsernames = document.getElementByClassName("username current");
+
+                // 取得 ID 为 myDiv 的元素中带有 class 为 selected 的所有元素
+                var selected = document.getElementById("mydiv").getElementByClassName("selected");
+
+            // 2. classList 属性
+                // 操作类名的传统方法
+                // <div class="bd user disabled">...</div>
+                // 删除 user
+                // 取得并拆分成数组
+                var classNames = div.className.split(/\s+/);
+
+                // 找到想删的类名
+                var pos = -1,
+                    i,
+                    len;
+                for (i = 0, len = classNames.length; i < len; i++)
+                {
+                    if (classnames[i] == "user")
+                    {
+                        pos = i;
+                        break;
+                    }
+                }
+
+                // 删除
+                classNames.splice(i, 1);
+
+                // 合并并重新设置
+                div.className = classNames.join(" ");
+
+                // HTML5 重写
+                div.classList.remove("user");
+
+                // 其他方法
+                // 删除 disabled 类
+                div.classList.remove("disabled");
+
+                // 添加 current 类
+                div.classList.add("current");
+
+                // 切换 user 类，也就是有删没添
+                div classList.toggle("user");
+
+                // 确定元素中有或没有该类名
+                if (div.classList.contains("bd") && !div.clasList.contains("disabled"))
+                {
+                    // 执行操作
+                }
+
+                // 迭代类名
+                for (var i = 0; i < div.classList.length; i++)
+                {
+                    doSomething(div.classList[i]);
+                }
+
+            // 11.3.2 焦点管理
+                // document.activeElement 属性始终引用 DOM 中获得了焦点的元素
+                var button = document.getElementById("myButton");
+                button.focus(); // 通过调用 focus 的方式让 button 获得焦点
+                console.log(document.activeElement === button);     // true
+
+                // document.hasFocus() 方法确认有没有获得焦点
+                var button = document.getElementById("myButton");
+                button.focus();
+                console.log(document.hasFocus());   // true
+
+            // 11.3.3 HTMLDocument 的变化
+                // 1.readyState 属性
+                    // 基本用法
+                    if (document.readyState == "complete")  // 加载中是 loading
+                    {
+                        // 某些在网页文档2加载完成后希望做的操作
+                    }
+
+                    // 较新的浏览器都支持
+
+                // 2.兼容模式是标准还是混杂的
+                    if (document.compatMode == "CSS1Compat")
+                    {
+                        console.log("Standards mode");  // 标准模式
+                    }
+                    else
+                    {
+                        console.log("Quirks mode"); // 混杂模式，值为 BackCompat
+                    }
+
+                // 3.head 属性
+                    // 类似于 document.body ，建议与后备方案一起用
+                    var head = document.head || document.getElementByTagname("head")[0];
+
+            // 11.3.4 字符集属性
